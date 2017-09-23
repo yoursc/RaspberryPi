05dbe4c8f4bbde247081e7e1407b4047b2446c7d
Yoursc <461047691@qq.com>
2017-09-24 01:31:50 +0800

    小幅修改 一键挂载SMB脚本

--- sh/auto_mount_smb.sh
+++ sh/auto_mount_smb.sh
@@ -1,18 +1,24 @@
 #!/bin/bash 
-#自动挂载、卸载网络samba文件
+#自动挂载(临时)、卸载网络samba文件
 
-DIR="/mnt/gee2"
-echo '挂载：极路由Samba'
-if [ -e $DIR/TDDOWNLOAD ]
+DIR="挂载路径"
+IPAD="192.168.199.98"
+SMB="smb服务名"
+USER="用户名"
+PASSWD='密码'
+
+###########代码区#############
+echo '挂载：Samba（pi）'
+if [ -d $DIR/ ]
 then
 	echo '设备已挂载'
 	sleep 0.5s
-	read -p '是否卸载：极路由Samba? (y/n) : '  -n 1
+	read -p '是否卸载：Samba(pi)? (y/n) : '  -n 1
 	echo ''
 	case $REPLY in
 	[y,Y])
 		echo '正在卸载'
-		sudo umount //192.168.199.2/120g
+		sudo umount //$IPAD/$SMB
 		[ $? = 0 ] && echo '卸载成功' || echo '卸载失败'
 	;;
 	*)
@@ -22,7 +28,7 @@ then
 else
 	sleep 0.5s
 	echo '正在检测网络通畅性'
-	ping -c 4 192.168.199.2 &> /dev/null
+	ping -c 4 $IPAD &> /dev/null
 	if [ $? != 0 ]
 	then
 		echo '网络连接失败！挂载终止！'
@@ -31,7 +37,7 @@ else
 		sleep 0.5s
 		echo '开始挂载'
 		[ ! -e $DIR ] && sudo mkdir -p $DIR
-		sudo mount //192.168.199.2/120g $DIR  -o user=root,passwd=passwd
+		sudo mount -t cifs //$IPAD/$SMB $DIR  -o user=$USER,password=$PASSWD
 		if [ $? = 0 ]
 		then
 			echo '挂载成功'
